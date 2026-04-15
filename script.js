2763e0994fc5f1c66e4077740b9086198a3979cc
Antonio Marques <antonyomarques2012@gmail.com>
2026-04-15 12:12:44 -0300

    v2

--- script.js
+++ script.js
@@ -32,13 +32,13 @@ let projetos = [
 {
     titulo: "Agente de Combate às Endemias",
     descricao: "Atuação no ensino de Física com foco em aprendizagem prática, resolução de problemas e desenvolvimento do pensamento crítico dos estudantes.",
-    imagem: "img/fotoagente.jpg",
+    imagem: "fotoagente.jpg",
     link: "https://github.com/antonyomarques2012-ops"
 },
 {
     titulo: "Professor de Física",
     descricao: "Atuação direta no controle e prevenção de doenças endêmicas, visitas domiciliares, orientações à população e suporte em campanhas de saúde pública.",
-    imagem: "img/professoraula.png",
+    imagem: "professoraula.png",
     link: "https://github.com/antonyomarques2012-ops"
 },
 
